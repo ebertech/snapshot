281f9b60d6b7e60bf0c70e7a4bb3dd041df5d7c4
Andrew Eberbach <andrew@ebertech.ca>
2012-02-25 19:46:12 -0500

    added better output

--- app/controllers/eber_tech/snapshot/list_revisions_command.rb
+++ app/controllers/eber_tech/snapshot/list_revisions_command.rb
@@ -1,10 +1,15 @@
 module EberTech
   module Snapshot
     class ListRevisionsCommand < AbstractCommand
-      def execute
+      def execute          
+        table = []
+        table << ["Revision (SHA1)", "Description"]
+        
         database.each_revision do |sha1, message|
-          puts "#{sha1}\t\t#{message}"          
-        end        
+          table << [sha1, message]
+        end
+        
+        Thor::Shell::Color.new.print_table table        
       end          
     end
   end
--- app/controllers/eber_tech/snapshot/list_tags_command.rb
+++ app/controllers/eber_tech/snapshot/list_tags_command.rb
@@ -2,9 +2,14 @@ module EberTech
   module Snapshot
     class ListTagsCommand < AbstractCommand
       def execute
+        table = []
+        table << ["Tag", "Description"]
+        
         database.each_tag do |tag, message|
-          puts "#{tag}\t\t#{message}"          
+          table << [tag, message]
         end
+        
+        Thor::Shell::Color.new.print_table table
       end          
     end
   end
